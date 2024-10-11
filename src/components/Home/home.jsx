9e501cb18171a48c449cf45c34aa84db50a729ea
Arundhati Singh <arundhatisingh171003@gmail.com>
2024-10-11 14:45:59 -0500

    added animation instead

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -15,42 +15,56 @@ function Home({isDarkMode}) {
     const intro_line = "Hi!";
     const intro_line2 = " My name is Arundhati Singh.";
 
-    useEffect(() => {
-        let index = 0;
-        const interval = setInterval(() => {
-            if (index < intro_line.length) {
-                setDisplayedText(intro_line.slice(0, index + 1));
-            }
-            index++;
-            if (index === intro_line.length) {
-                clearInterval(interval);
-            }
-        }, 100);
-        return () => clearInterval(interval);
-    }, [intro_line]);
+    // useEffect(() => {
+    //     let index = 0;
+    //     const interval = setInterval(() => {
+    //         if (index < intro_line.length) {
+    //             setDisplayedText(intro_line.slice(0, index + 1));
+    //         }
+    //         index++;
+    //         if (index === intro_line.length) {
+    //             clearInterval(interval);
+    //         }
+    //     }, 100);
+    //     return () => clearInterval(interval);
+    // }, [intro_line]);
 
-    useEffect(() => {
-        if (displayedText === intro_line) {
-            let index = 0;
-            const interval = setInterval(() => {
-                if (index < intro_line2.length) {
-                    setDisplayedText1(intro_line2.slice(0, index + 1));
-                }
-                index++;
-                if (index === intro_line2.length) {
-                    clearInterval(interval);
-                }
-            }, 100);
-            return () => clearInterval(interval);
-        }
-    }, [displayedText, intro_line2]);
+    // useEffect(() => {
+    //     if (displayedText === intro_line) {
+    //         let index = 0;
+    //         const interval = setInterval(() => {
+    //             if (index < intro_line2.length) {
+    //                 setDisplayedText1(intro_line2.slice(0, index + 1));
+    //             }
+    //             index++;
+    //             if (index === intro_line2.length) {
+    //                 clearInterval(interval);
+    //             }
+    //         }, 100);
+    //         return () => clearInterval(interval);
+    //     }
+    // }, [displayedText, intro_line2]);
 
     return (
       <Container id="hello">
         <Card className= {`bg-transparent border-0 ${isDarkMode ? 'dark' : ''}`}>
           <Card.Body className = {`flip`}>
-            <h1>{displayedText}</h1>
-            <h1>{displayedText1}</h1>
+            {/* <h1>{displayedText}</h1>
+            <h1>{displayedText1}</h1> */}
+            <h1>
+              {intro_line.split('').map((char, index) => (
+                <span key={index} className="fade-in" style={{ animationDelay: `${index * 0.1}s` }}>
+                  {char}
+                </span>
+              ))}
+            </h1>
+            <h1>
+              {intro_line2.split('').map((char, index) => (
+                <span key={index} className="fade-in" style={{ animationDelay: `${(intro_line.length + index) * 0.1}s` }}>
+                  {char}
+                </span>
+              ))}
+            </h1>
             <p id = "about_me">I am a full-stack developer, software engineer, and ML researcher. I love envisioning, prototyping, and implementing my ideas
                 into real-world applications. I am also working as an undergraduate research assistant in the FabLab at the University of Wisconsin-Madison where 
                 I am working on developing a program that communicates with a 3D-metal printer to print 3D titanium models. I am also creating an online platform called DevPals that connects 
